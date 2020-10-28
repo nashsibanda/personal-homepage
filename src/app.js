196375c92067ed3ff76c0a7537f69cfd0651fada
Nash Sibanda <contact@nashsibanda.com>
2020-10-28 13:37:50 +0000

    Initial commit for react app

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from "react";
+import NavMenu from "./components/nav_menu";
+
+const App = () => (
+  <div className="site-container">
+    <p>Hello world!</p>
+    <NavMenu />
+  </div>
+);
+
+export default App;
---
+++ src/components/nav_menu.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const NavMenu = () => (
+  <nav>
+    <ul>
+      <li>
+        <a href="#home">Home</a>
+      </li>
+      <li>
+        <a href="#projects">Projects</a>
+      </li>
+      <li>
+        <a href="#blog">Blog</a>
+      </li>
+    </ul>
+  </nav>
+);
+
+export default NavMenu;
