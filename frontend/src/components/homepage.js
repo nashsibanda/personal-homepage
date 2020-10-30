feec43a007f394e0ae822ba5695f3131d32168fe
Nash Sibanda <contact@nashsibanda.com>
2020-10-30 01:49:20 +0000

    Render homepage is conditional on user loaded

--- frontend/src/components/homepage.js
+++ frontend/src/components/homepage.js
@@ -2,9 +2,13 @@ import React from "react";
 
 const Homepage = ({ user }) => (
   <main className="homepage">
-    <img src={user.logo} alt={`Site logo for ${user.siteUrl}`}></img>
-    <h1>{user.name}</h1>
-    <h2>{user.headline}</h2>
+    {user && (
+      <>
+        <img src={user.logo} alt={`Site logo for ${user.siteUrl}`}></img>
+        <h1>{user.name}</h1>
+        <h2>{user.headline}</h2>
+      </>
+    )}
   </main>
 );
 
