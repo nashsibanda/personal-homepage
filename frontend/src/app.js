454bead749f87a44d446b82b30deef9ef1f62965
Nash Sibanda <contact@nashsibanda.com>
2020-10-30 22:22:24 +0000

    Add react-transition-group, add slide-out to main sections

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -2,25 +2,21 @@ import React, { useEffect, useState } from "react";
 import ApiService from "./services/api_service";
 import Footer from "./components/footer/footer_main";
 import Homepage from "./components/homepage";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  withRouter,
+} from "react-router-dom";
 import ProjectsIndex from "./components/projects/projects_index";
 import ProjectShow from "./components/projects/project_show";
 import AboutPage from "./components/about";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
-const App = () => {
-  const [user, setUser] = useState(null);
-  const [projects, setProjects] = useState(null);
-
-  useEffect(
-    () =>
-      ApiService.projects.getProjects().then(projects => setProjects(projects)),
-    []
-  );
-  useEffect(() => ApiService.users.getUser().then(user => setUser(user)), []);
-
-  return (
-    <Router>
-      <Switch>
+const AnimatedRouteSwitch = withRouter(({ location, user, projects }) => (
+  <TransitionGroup>
+    <CSSTransition key={location.key} classNames="slide" timeout={1000}>
+      <Switch location={location}>
         <Route path="/projects/:projectSlug">
           {projects && <ProjectShow projects={projects} />}
         </Route>
@@ -35,6 +31,24 @@ const App = () => {
           <Homepage user={user} />
         </Route>
       </Switch>
+    </CSSTransition>
+  </TransitionGroup>
+));
+
+const App = () => {
+  const [user, setUser] = useState(null);
+  const [projects, setProjects] = useState(null);
+
+  useEffect(
+    () =>
+      ApiService.projects.getProjects().then(projects => setProjects(projects)),
+    []
+  );
+  useEffect(() => ApiService.users.getUser().then(user => setUser(user)), []);
+
+  return (
+    <Router>
+      <AnimatedRouteSwitch user={user} projects={projects} />
       <Footer user={user} />
     </Router>
   );
