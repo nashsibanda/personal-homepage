0557600a8a9b4302b052f64d60be789db43ab355
Nash Sibanda <contact@nashsibanda.com>
2020-10-31 01:26:36 +0000

    Add carousel to gallery, with css arrow buttons

--- frontend/src/components/gallery.js
+++ frontend/src/components/gallery.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 const getYoutubeId = url => {
   // A more comprehensive regex, but that doesn't seem to work quite right.
@@ -12,32 +13,73 @@ const getYoutubeId = url => {
 
 const Gallery = ({ gallery }) => {
   const [currentItem, setCurrentItem] = useState(gallery[0]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const carousel = gallery.length > 5;
 
   const setItem = url => {
-    return () => setCurrentItem(gallery.find(item => item.url === url));
+    return () => {
+      setCurrentItem(gallery.find(item => item.url === url));
+      setCurrentIndex(gallery.findIndex(item => item.url === url));
+    };
+  };
+
+  const galleryCarouselItems = () => {
+    const lowerIndex =
+      currentIndex - 2 > 0
+        ? currentIndex - 2
+        : (gallery.length + (currentIndex - 2)) % gallery.length;
+    const upperIndex = (currentIndex + 2) % gallery.length;
+    const outputItems = carousel
+      ? [
+          gallery[lowerIndex],
+          gallery[(lowerIndex + 1) % gallery.length],
+          gallery[currentIndex],
+          gallery[
+            (upperIndex - 1 >= 0
+              ? upperIndex - 1
+              : gallery.length + (upperIndex - 1)) % gallery.length
+          ],
+          gallery[upperIndex],
+        ]
+      : gallery;
+
+    console.log(outputItems);
+    return outputItems;
   };
 
   const { type, url, description } = currentItem;
   return (
     <div className="gallery">
       <div className="current-item">
-        {type === "image" ? (
-          <img src={url} alt={description} />
-        ) : (
-          <iframe
-            src={`https://www.youtube-nocookie.com/embed/${getYoutubeId(
-              url
-            )}?showinfo=0&rel=1&color=white&autoplay=1&modestbranding=1`}
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-            title={description}
-          ></iframe>
-        )}
+        <TransitionGroup>
+          <CSSTransition key={url} classNames="slide" timeout={1000}>
+            {type === "image" ? (
+              <img src={url} alt={description} />
+            ) : (
+              <iframe
+                src={`https://www.youtube-nocookie.com/embed/${getYoutubeId(
+                  url
+                )}?showinfo=0&rel=1&color=white&autoplay=1&modestbranding=1`}
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                title={description}
+              ></iframe>
+            )}
+          </CSSTransition>
+        </TransitionGroup>
       </div>
       <div className="image-description">{description}</div>
       <ul>
-        {gallery.map(item => (
+        {carousel && (
+          <li
+            className="carousel-arrow left"
+            onClick={setItem(galleryCarouselItems()[1].url)}
+          >
+            <div></div>
+          </li>
+        )}
+        {galleryCarouselItems().map(item => (
           <li key={item.url} onClick={setItem(item.url)}>
             <img
               src={
@@ -49,6 +91,16 @@ const Gallery = ({ gallery }) => {
             />
           </li>
         ))}
+        {carousel && (
+          <li
+            className="carousel-arrow right"
+            onClick={setItem(
+              galleryCarouselItems()[galleryCarouselItems().length - 2].url
+            )}
+          >
+            <div></div>
+          </li>
+        )}
       </ul>
     </div>
   );
